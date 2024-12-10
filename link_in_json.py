f165102c020ac2ba4a07a22336bb2e2b97d24f9f
Kerestis <artem.brodovsky@gmail.com>
2024-12-10 13:06:04 +0300

    скрипт создания json файла

---
+++ link_in_json.py
@@ -0,0 +1,13 @@
+import json
+
+link_on_playlist = {'Anger': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577012',
+                    'Contempt': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577125',
+                    'Disgust': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84576947',
+                    'Fear': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577148',
+                    'Happy': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577075',
+                    'Neutreal': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577345',
+                    'Sad': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84576958',
+                    'Surprised': 'https://vk.com/music/curator/mzk/playlists?z=audio_playlist-34384434_84577343'}
+
+with open("link_for_playlist.json", "w", encoding="utf-8") as json_file:
+    json.dump(link_on_playlist, json_file, ensure_ascii=False, indent=4)
